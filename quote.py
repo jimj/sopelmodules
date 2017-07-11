cfc0cec5f07699c6dfb38d69c7f5138b3a86ee6f
Jim Johnson <github@jimj.net>
2017-07-11 10:08:51 -0700

    Minor readability improvements.

--- quote.py
+++ quote.py
@@ -3,7 +3,23 @@ import re
 
 from sopel.module import commands, example
 
-nick_pattern = '(<[^\s]+>)' #Pattern for <@+nickNames>
+#Matches: <@+nickNames123>
+_NICKNAME = '(<[^\s]+>)'
+
+@commands('quote')
+def quote(bot, trigger):
+    quotes = bot.mongodb.quotes
+    input = trigger.group(2) if trigger.group(2) else ''
+
+    quotable = re.findall(_NICKNAME, input)
+
+    if quotable:
+        who, quote = parse_quotable_params(input)
+        store_quote(quotes, who, quote)
+        bot.reply('quote stored.')
+    else:
+        nick, search = parse_search_params(input)
+        bot.say(get_random_quote(quotes, nick, search))
 
 def parse_search_params(params):
     param_re = '(?P<nick>\w+)?\s?(?P<search>\/.*\/)?$'
@@ -24,7 +40,7 @@ def parse_quotable_params(params):
     quoted = [params.group('realname')] if params.group('realname') else []
     quote = params.group('quote')
 
-    nicks = re.findall(nick_pattern, quote)
+    nicks = re.findall(_NICKNAME, quote)
     if  nicks:
         nicks = [re.subn(r'[@<>+]', '', n)[0] for n in nicks]
         quoted = [nick.lower() for nick in set(quoted + nicks)]
@@ -37,15 +53,15 @@ def get_random_quote(quotes, nick, search):
         query['nick'] = nick.lower()
     if search:
         query['quote'] = re.compile(search, re.IGNORECASE)
-    
+
     ids = quotes.find(query, [])
     num_quotes = ids.count()
-    
+
     if num_quotes == 0:
         return "No quotes found."
 
     quote = random.randint(0, num_quotes - 1)
-    
+
     quote = quotes.find({'_id': ids[quote]['_id']}, ['quote'])
     return quote[0]['quote']
 
@@ -57,19 +73,3 @@ def store_quote(quotes, quoted, quote):
     }
 
     quotes.insert(quote_doc)
-
-@commands('quote')
-def quote(bot, trigger):
-    quotes = bot.mongodb.quotes
-    input = trigger.group(2) if trigger.group(2) else ''
-
-    #if the input contains <Something>, assume it's an input quote 
-    quotable = re.findall(nick_pattern, input)
-
-    if quotable:
-        who, quote = parse_quotable_params(input)
-        store_quote(quotes, who, quote)
-        bot.reply('quote stored.')
-    else:
-        nick, search = parse_search_params(input)
-        bot.say(get_random_quote(quotes, nick, search))
