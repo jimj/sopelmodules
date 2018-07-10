c8caed5c0ee629066ab7c312f6d32b39fa5fb179
Jim Johnson <github@jimj.net>
2018-07-09 17:44:13 -0700

    Add user-agent spoof.

--- squote.py
+++ squote.py
@@ -9,7 +9,7 @@ def lookup(bot, trigger):
     ticker_symbol = trigger.group(2).upper()
     url = _URL % ticker_symbol
 
-    response = requests.get(url)
+    response = requests.get(url, headers={'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'})
     if response.status_code != 200:
         bot.say("Got HTTP %s trying to look up %s" % (response.status_code, ticker_symbol))
     else:
